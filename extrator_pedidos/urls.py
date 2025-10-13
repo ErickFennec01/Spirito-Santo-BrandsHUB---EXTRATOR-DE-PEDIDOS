5f9e5deab1ec27b67974bdd77a8425e6e243a7ff
ErickFennec01 <166094257+ErickFennec01@users.noreply.github.com>
2025-10-13 14:17:53 -0300

    Update urls.py

--- extrator_pedidos/urls.py
+++ extrator_pedidos/urls.py
@@ -1,7 +1,8 @@
-# extrator_pedidos/urls.py
 from django.urls import path
-from .views import index, ProcessarPedido
+from .views import index, ProcessarPedido, health_check # Adicione 'health_check' aqui
+
 urlpatterns = [
     path('', index, name='index'),
     path('processar_pedido/', ProcessarPedido.as_view(), name='processar_pedido'),
+    path('health/', health_check, name='health_check'), # Adicione esta linha
 ]
